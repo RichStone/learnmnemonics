5bfbc53757c89bc26f81834fa6425c5ca0c44138
Rich Steinmetz <richard.had@hotmail.de>
2022-06-04 12:36:03 +0200

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Default Major System Pegs.
+%w[
+    hose hat hen home arrow whale shoe cow hoof pie
+    daisy tattoo tuna dome diary tail dish dog dove tuba
+    nose net onion enemy winery nail nacho neck knife honeybee
+  ].each_with_index do |i, phrase|
+
+  peg = MajorSystemPeg.create(
+    number: i,
+    phrase: phrase
+  )
+  peg.image.attach(io: File.open("storage/seeds/bob.jpeg"), filename: "bob.jpeg")
+end
