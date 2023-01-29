6f30baebba05ca4e05791138997edfaddde8f8f7
Rich Steinmetz <richard.had@hotmail.de>
2023-01-29 08:05:47 +0100

    Fix integrations test

--- test/controllers/brain/major_systems_controller_test.rb
+++ test/controllers/brain/major_systems_controller_test.rb
@@ -3,20 +3,16 @@ require "test_helper"
 class Brain::MajorSystemsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test "should get index" do
-    skip "seems not to sign in properly in integration tests"
-    # Minitest::Assertion: Expected response to be a <2XX: success>, but was a <302: Found> redirect to <http://www.example.com/brains/sign_in>
+  setup do
+    sign_in(brains(:rich))
+  end
 
-    sign_in(:rich)
+  test "should get index" do
     get brain_major_systems_url
     assert_response :success
   end
 
   test "should get show" do
-    skip "seems not to sign in properly in integration tests"
-    # Minitest::Assertion: Expected response to be a <2XX: success>, but was a <302: Found> redirect to <http://www.example.com/brains/sign_in>
-
-    sign_in(:rich)
     get brain_major_systems_url
     assert_response :success
   end
